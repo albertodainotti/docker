10f5923b70a3cb4db7097d533847b9e3f76611bd
Alberto Dainotti <alberto@caida.org>
2021-04-08 21:13:57 -0700

    added some customization to choose distro/release

--- ubuntu-dev/buildme.sh
+++ ubuntu-dev/buildme.sh
@@ -1,2 +1,26 @@
-#!/bin/sh
-docker build -t ubuntu-dev:bionic -t ubuntu-dev:latest ./
+#!/bin/bash
+set -e
+
+# Defaults for command line arguments
+DISTRO="ubuntu"		# ubuntu, debian
+RELEASE="latest" 	# latest, buster-slim, ...
+MYFLAVOR="slim"		# slim vs devel
+
+# Other defaults
+USER="alberto"
+
+if [[ $# > 0 ]]; then
+	DISTRO="$1"
+	if [[ $# > 1 ]]; then
+		RELEASE="$2"
+		if [[ $# > 2 ]]; then
+			MYFLAVOR="$3"
+		fi
+	fi
+fi
+
+
+NAME=${DISTRO}:${RELEASE}
+echo "Creating image for NAME=${DISTRO}:${RELEASE} with tag ${DISTRO}-my${MYFLAVOR}-${RELEASE}"
+
+docker build -t "${DISTRO}-my${MYFLAVOR}-${RELEASE}" --build-arg name=${NAME} --build-arg distro=${DISTRO} --build-arg distro-release=${RELEASE} --build-arg user=${USER}  --build-arg myflavor=${MYFLAVOR} ./
